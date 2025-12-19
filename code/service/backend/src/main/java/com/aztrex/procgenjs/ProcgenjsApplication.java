6b5aff2cb214ceec9a3f5139f27b9e7039eea116
aztrexdx <aztrexdx@gmail.com>
2025-12-19 16:42:41 +0530

    feat: Set up initial Spring Boot backend service for procgenjs.

---
+++ code/service/backend/src/main/java/com/aztrex/procgenjs/ProcgenjsApplication.java
@@ -0,0 +1,13 @@
+package com.aztrex.procgenjs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcgenjsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcgenjsApplication.class, args);
+	}
+
+}
---
+++ code/service/backend/src/test/java/com/aztrex/procgenjs/ProcgenjsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aztrex.procgenjs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcgenjsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
